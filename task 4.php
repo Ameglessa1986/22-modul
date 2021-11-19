8dd89defbda1fc05e095fd0274384bc2bc749af7
Alekseeva Anastasiya <faya-1023@mail.ru>
2021-11-19 08:59:19 +0300

    initial commit

---
+++ task 4.php
@@ -0,0 +1,50 @@
+<!-- 
+Задача №3188. Дуб, орех или пшено.
+
+Вводится символ.
+Входные данные
+
+Вводится символ.
+D - дуб
+O - орех
+P - пшено
+N - ничего
+
+Выходные данные
+Если пользователь выбрал орех то вывести:
+Na kogo greh?
+Если пшено:
+Mi s tobou za odno!
+Если дуб:
+Tri raza v zub!
+Если ничего:
+Viberay!
+(См. пример).
+
+Примеры
+входные данные
+D
+выходные данные
+Tri raza v zub!
+ -->
+
+ <?php
+
+$char = readline();
+
+switch($char){
+
+    case("D"):
+        echo "Tri raza v zub!";
+        break;
+    case("O"):
+        echo "Na kogo greh?";
+        break;
+    case("P"):
+        echo "Mi s tobou za odno!";
+        break;
+
+    default: 
+        echo "Viberay!";
+        break;
+}
